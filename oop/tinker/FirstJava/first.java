8d5e97dfe064ce661d6dccc6546888dd7724cf09
Taharka <okait001.309@gmail.com>
2020-01-30 01:02:04 +0000

    > [Lazygit]

---
+++ oop/tinker/FirstJava/first.java
@@ -0,0 +1,10 @@
+package oop.tinker.FirstJava;
+
+class FirstJava {
+  public static void main(String args[]) {
+    System.out.println("First Java!");
+    for (String arg : args) {
+      System.out.println(arg);
+    }
+  }
+};
