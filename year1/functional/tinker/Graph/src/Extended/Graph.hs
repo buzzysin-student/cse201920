b5d02ebc7de9d824af2feea896dbfd062a4ffe36
buzzysin <okait001.309@gmail.com>
2021-02-09 14:41:09 +0000

    > Forgot to commit

--- year1/functional/tinker/Graph/src/Extended/Graph.hs
+++ year1/functional/tinker/Graph/src/Extended/Graph.hs
@@ -1,20 +1,20 @@
 module Extended.Graph where
 
 import Base (Graph, graphEdges, graphFromEdges, graphVertices)
+import Edge (edgePairs)
 import Extended.Edge (pieNormalise)
 import Extended.Vertex (pivNormalise)
 import Test.QuickCheck (Arbitrary (arbitrary), Positive, getPositive)
 import Vertex (Vertex (..), vertexValue)
-import Edge (edgePairs)
 
 instance (Eq vs, Eq es, Arbitrary vs, Arbitrary es) => Arbitrary (Graph vs es) where
   arbitrary = do graphFromEdges <$> arbitrary
 
 newtype PIGraph = PIGraph {getPIGraph :: Graph (Positive Integer) (Positive Integer)}
 
-pigNormalise pig = Graph vs es
-  where
-    es = error
-    pig' = getPIGraph pig
-    es' = map (edgePairs) (graphEdges pig')
-    vs = map (Vertex . getPositive . vertexValue) (graphVertices pig')
+-- pigNormalise pig = Graph vs es
+--   where
+--     es = error
+--     pig' = getPIGraph pig
+--     es' = map (edgePairs) (graphEdges pig')
+--     vs = map (Vertex . getPositive . vertexValue) (graphVertices pig')
--- year1/imperative/cw/3/test.sh
+++ year1/imperative/cw/3/test.sh
@@ -1,2 +1,2 @@
-compile -rmc test.c -i"sketch.c -DTESTING" ${@:2}
+compile -rmc test.c -i"I/usr/include/SDL2 sketch.c -DTESTING" ${@:2}
 echo ${@:2}
